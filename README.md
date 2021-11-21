30acfab6e2239b6ef35e5588f79b72b06b58a84f
Luke <lukeconrad.stew@gmail.com>
2021-11-20 16:05:13 -0800

    got for loop to work properly: had to make true statments to match my varibles: updates the read me with pictures and links

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# password-generator
+Task was to build a random password generator with values that we choose,
+had to write the Javascript, HTML & CSS was given pior. 
+Give a list of prompted questions you must choose, the length, and what characters you want
+to include. 
+Genterate button will deploy a password with the length you choose and the random character values. 
+
+![text](generator.png)
+
+I learned how to create a for loop for a single output. 
+I learned how to combine arrays into a single array.
+I had to check my varibles if they were true in order to work.
+
--- script.js
+++ script.js
@@ -1,68 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperCaseArr= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var lowerCaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var specialCharArr = ["!","@","#","$","%","^","&","*","(",")","<",">","?"]
-var numbersArr = ["1","2","3","4","5","6","7","8","9","0"]
+var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?"];
+var numbersArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-// Write password to the #password input,
+// critera for the length of writing password
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-      
-  if (password === false) {
-    writePassword()
-  } else {
-    passwordText.value = password;
+  var lengthSize = parseInt(prompt("Choose desired password length (8-128)"))
+  console.log(lengthSize)
+  if (lengthSize < 8 || lengthSize > 128) {
+    alert("Choose a number between (8-128)")
+    return;
   }
+
+  // User Seletion for password options
+  var upperCase = confirm("Do you want uppercase characters?")
+  console.log(upperCase)
+  var lowerCase = confirm("Do you want lowerCase characters?")
+  console.log(lowerCase)
+  var specialChar = confirm("Do you want special characters?")
+  console.log(specialChar)
+  var numbers = confirm("Do you want number(s) characters?")
+  console.log(numbers)
+
+  if (upperCase === false && lowerCase === false && specialChar === false && numbers === false) {
+    alert("Must have at least 1 of the following options included");
+    return "Try again";
+  }
+
+  // combining arrays for possible password outcome
+  var possiblePass = [];
+  var finalPass = "";
+
+  if (upperCase === true) {
+    possiblePass = possiblePass.concat(upperCaseArr);
+  }
+  if (lowerCase === true) {
+    possiblePass = possiblePass.concat(lowerCaseArr);
+  }
+  if (specialChar === true) {
+    possiblePass = possiblePass.concat(specialCharArr);
+  }
+  if (numbersArr === true) {
+    possiblePass = possiblePass.concat(numbersArr);
+  }
+   // Possible password selection 
+  for (var i = 0; i < lengthSize; i++) {
+    finalPass = finalPass+possiblePass[Math.floor(Math.random() * possiblePass.length)];
+    document.querySelector("#password").placeholder=finalPass;
+  }
+  console.log(possiblePass);
 }
-
-  
-  
-
-
-//  Generate length and random possible passwords
-function generatePassword() {
- var lengthSize = prompt ("Choose desired password length (8-128)")
- console.log (length)
- if (lengthSize < 8 || lengthSize > 128) {
-   alert ("Choose a number between 8-128")
-   return "Try again"
- } 
-   
-// User Seletion
- var upperCase = confirm ("Do you want uppercase characters?")
- console.log (upperCase)
- var lowerCase = confirm ("Do you want lowerCase characters?")
- console.log (lowerCase)
- var specialChar = confirm ("Do you want special characters?")
- console.log (specialChar)
- var numbers = confirm ("Do you want number(s) characters?")
- console.log (numbers)
- if (upperCase === false && lowerCase === false && specialChar === false && numbers === false)
- alert ("Must have at least 1 of the following options included")
- return "Try again"
-}
-
-// Possible password selection 
-var possiblePass
-var finalPass
-
-if (lowerCase) {
-  possiblePass = possiblePass.concat(lowerCaseArr)
-}
-if (upperCase) {
-  possiblePass = possiblePass.concat(upperCaseArr)
-}
-if (lowerCase) {
-  possiblePass = possiblePass.concat(numbersArr)
-}
-if (lowerCase) {
-  possiblePass = possiblePass.concat(specialCharArr)
-}
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
